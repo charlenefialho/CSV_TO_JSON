1ea140ba5b930570f398a9bfc85ae43b1d2d23e0
charlenefialho <fialhocharlene@gmail.com>
2023-06-23 07:40:58 -0300

    refactor: split the function convertCsvToJson into readCsvFile and createDownloadLink

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 const inputFile = document.querySelector("#input-file");
 const outputJson = document.querySelector("#output-json");
 
-inputFile.addEventListener('change', convertCsvToJson);
+inputFile.addEventListener('change', readCsvFile);
 
-function convertCsvToJson(event) {
+function convertCsvToJson(lines, header) {
+  const data = [];
+  for (let i = 1; i < lines.length - 1; i++) {
+    const columns = lines[i].split(',');
+    const obj = {};
+
+    for (let j = 0; j < header.length; j++) {
+      obj[header[j]] = columns[j];
+    }
+
+    data.push(obj);
+  }
+  outputJson.innerText = JSON.stringify(data);
+
+    downloadJsonFile(createDownloadLink(JSON.stringify(data)));
+}
+
+
+function readCsvFile(event){
   const file = event.target.files[0];
 
   const reader = new FileReader();
   reader.onload = function(e) {
     const content = e.target.result;
-
     const lines = content.split('\n');
     const header = lines[0].split(',');
-    const data = [];
 
-    for (let i = 1; i < lines.length - 1; i++) {
-      const columns = lines[i].split(',');
-      const obj = {};
+    convertCsvToJson(lines, header);
+  };
+  reader.readAsText(file);
 
-      for (let j = 0; j < header.length; j++) {
-        obj[header[j]] = columns[j];
-      }
+}
 
-      data.push(obj);
-    }
-    outputJson.innerText = JSON.stringify(data);
 
-    const jsonData = JSON.stringify(data);
-    const blob = new Blob([jsonData], { type: 'application/json' });
+function downloadJsonFile(linkDownload){
+  linkDownload.dispatchEvent(new MouseEvent('click'));
+}
+
+function createDownloadLink(jsonData){
+  const blob = new Blob([jsonData], { type: 'application/json' });
 
     const downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.download = 'data.json';
 
-    downloadLink.dispatchEvent(new MouseEvent('click'));
-  };
-
-  reader.readAsText(file);
+    return downloadLink;
 }
